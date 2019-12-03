14174257949fc7f3dd82bad2ca5411b2a75a47ce
Royce <rosc1351@colorado.edu>
2019-12-03 01:01:20 -0700

    adds basic label functionality. Can set and remove labels. Can list all messages with specified label

--- model.py
+++ model.py
@@ -6,13 +6,51 @@ class Gmail:
     def __init__(self, credentials='credentials.json'):
         self.service = LogIn(credentials)
         self.next_page = None
-        self.messages = self.getMessages()
+        self.getMessages()
         self.msg_idx = 0
 
-    def getLabels(self):
-        results = self.service.users().labels().list(userId='me').execute()
-        labels = results.get('labels', [])
-        return labels
+    def getLabels(self, msgId=None):
+        if msgId:
+            message = self.service.users().messages().get(userId='me', id=msgId).execute()
+            return message['labelIds']
+        else:
+            results = self.service.users().labels().list(userId='me').execute()
+        return results.get('labels',[])
+
+    def setLabel(self, msgId, label, add=True):
+        try:
+            print(label['id'])
+            if add:
+                body = { "addLabelIds": [label['id']],
+                "removeLabelIds" : []}
+            else:
+                print('here')
+                body = {"addLabelIds": [],
+                "removeLabelIds" : [label['id']]}
+            print(body)
+            message = self.service.users().messages().modify(userId='me', id=msgId, body=body).execute()
+            return message
+        except:
+            return 'error'
+
+    def ListMessagesWithLabels(self, label_ids=[]):
+        try:
+            response = self.service.users().messages().list(userId='me',
+                                                   labelIds=label_ids).execute()
+            messages = []
+            if 'messages' in response:
+              messages.extend(response['messages'])
+
+            while 'nextPageToken' in response:
+              page_token = response['nextPageToken']
+              response = self.service.users().messages().list(userId='me',
+                                                         labelIds=label_ids,
+                                                         pageToken=page_token).execute()
+              messages.extend(response['messages'])
+
+            return messages
+        except:
+            return 'error'
 
     def getMessages(self, reset=False):
         if (not reset) and self.next_page=='End':
